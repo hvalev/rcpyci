3c78874c878ea682c044f24569422892485fc934
Hristo Valev <h_valev@yahoo.de>
2024-05-06 17:03:57 +0200

    updated setup script for installing env for testing R and python reference implementation

--- tests/setup_pyenv.sh
+++ tests/setup_pyenv.sh
@@ -3,6 +3,6 @@
 python3 -m venv pyrcicr
 source pyrcicr/bin/activate
 # install requirements
-python3 -m pip install -r /tests/pyrcicr_ref/requirements.txt
+python3 -m pip install -r /tests/pyrcicr_ref/requirements_rpy2.txt
 # install the reference implementation package
 python3 -m pip install /tests/pyrcicr_ref/
