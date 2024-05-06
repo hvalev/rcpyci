3fedf0f51957175da917dfb27288f547b4e44b7f
Hristo Valev <h_valev@yahoo.de>
2024-05-06 21:49:07 +0200

    init for standard experiment configurations

--- rcpyci/consts.py
+++ rcpyci/consts.py
@@ -1,1 +1,16 @@
 ### file for containing standard values for certain functions
+baseline_params_kwargs = {
+    'n_trials': 5,
+    'n_scales': 5,
+}
+
+# fetch and overwrite params when needed
+default_pipeline_kwargs = {
+    'scaling': 'independent',
+    'scaling_constant': 0.1,
+    'mask': None
+}
+
+quick_zmap_kwargs = {}
+
+ttest_zmap_kwargs = {}
