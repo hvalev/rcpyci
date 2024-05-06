75e7758c2b036d2b8f6ab355e4a257d0f6b0d07e
Hristo Valev <h_valev@yahoo.de>
2024-05-06 17:17:56 +0200

    init for test file

---
+++ rcpyci/tests/test_X.py
@@ -0,0 +1,4 @@
+# TODO add tests for functions
+# TODO add a pyproject.toml
+# TODO add ref implementation as [dev] dependency
+# TODO idempotence tests of current against the reference implementation
