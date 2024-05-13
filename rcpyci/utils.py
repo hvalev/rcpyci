9ac744071de528f3daa24d9f2bf0a92acc977f8f
Hristo Valev <h_valev@yahoo.de>
2024-05-13 17:12:21 +0200

    added function to verify data

--- rcpyci/utils.py
+++ rcpyci/utils.py
@@ -27,3 +27,52 @@ def create_test_data(num_participants:int=100, num_stimulus:int=770):
         'responses': np.random.choice([-1, 1], size=num_participants*num_stimulus).astype(int),
     }
     return pd.DataFrame(data)
+
+def vprint(msg, verbose=False):
+    if verbose:
+        print(msg)
+
+def verify_data(df: pd.DataFrame, verbose=False):
+    # Step 1: Check dimensions
+    vprint("Dimensions of DataFrame:", verbose)
+    vprint(df.shape, verbose)
+
+    # Report on missing values
+    vprint("\nMissing values:", verbose)
+    vprint(df.isnull().sum(), verbose)
+    if len(df.isnull().sum().unique()) != 1:
+        print("ERROR identified missing values. Please run validation with verbose=True")
+
+    # Establish facts about the dataset
+    vprint("\nDistribution of participant IDs:", verbose)
+    vprint(df['participant_id'].value_counts(), verbose)
+    print(f"Found {len(df['participant_id'].value_counts())} participants")
+    if len(df['participant_id'].value_counts().unique()) == 1:
+        n_trials = list(df['participant_id'].value_counts().unique())[0]
+        print(f"Found {n_trials} trials per participants")
+    
+    # Step 3: Check distribution of conditions, participant IDs, and stimulus IDs
+    vprint("\nDistribution of conditions:", verbose)
+    vprint(df['condition'].value_counts(), verbose)
+    if len(df['condition'].value_counts().unique()) != 1:
+        print('WARNING conditions are not even between participants')
+    
+
+    vprint("\nDistribution of stimulus IDs:", verbose)
+    vprint(df['stimulus_id'].value_counts(), verbose)
+    if len(df['stimulus_id'].value_counts().unique()) != 1:
+        print('ERROR not all stimuli have been presented evenly to participants')
+
+    if 'stimulus_presentation_order' in df.columns:
+        vprint("\nDistribution of stimulus IDs:", verbose)
+        vprint(df['stimulus_presentation_order'].value_counts(), verbose)
+        if len(df['stimulus_presentation_order'].value_counts().unique()) != 1:
+            print('ERROR presentation order id not congruent with stimuli')
+
+    # Step 5: Assess response distribution
+    vprint("\nDistribution of responses:", verbose)
+    vprint(df['responses'].value_counts(), verbose)
+    responses = df['responses'].unique()
+    if not (len(responses) == 2 and -1 in responses and 1 in responses):
+        print('ERROR: Found a value in responses different from 1 or -1')
+
