c87650a9141e10d9f4f2469804e5cf4e83aa1776
Hristo Valev <h_valev@yahoo.de>
2024-05-13 17:15:19 +0200

    added unified way of computing ci and zmaps split by participant or condition

--- rcpyci/interface.py
+++ rcpyci/interface.py
@@ -24,6 +24,215 @@ from utils import create_test_data, verify_data, skip_if_exist
 
 logging.basicConfig(level=logging.INFO)
 
+
+def analyze_data(data: pd.DataFrame,
+                 base_face_path:str,
+                 stimuli_params: np.ndarray = None,
+                 ci_postproc_pipe=ci_postprocessing_pipeline,
+                 ci_postproc_kwargs=default_ci_postprocessing_pipeline_kwargs,
+                 zmap_pipe=compute_zmap_ttest_pipeline,
+                 zmap_kwargs=default_compute_zmap_ttest_pipeline_kwargs,
+                 anti_ci=False,
+                 n_trials: int = 770,
+                 n_scales: int = 5,
+                 sigma: int = 5,
+                 noise_type: str = 'sinusoid',
+                 seed: int = 1,
+                 save_ci: bool = True,
+                 save_zmap: bool = True,
+                 experiment_path:str='./experiment',
+                 label:str='rcpyci',
+                 n_jobs=10):
+
+    @skip_if_exist
+    def process_condition(condition,
+                          data: pd.DataFrame,
+                          base_image: np.ndarray,
+                          stimuli_params: np.ndarray = None,
+                          ci_postproc_pipe: Callable[[Any], Any] = ci_postprocessing_pipeline,
+                          ci_postproc_kwargs: dict = default_ci_postprocessing_pipeline_kwargs,
+                          zmap_pipe: Callable[[Any], Any] = compute_zmap_ttest_pipeline,
+                          zmap_kwargs: dict = default_compute_zmap_ttest_pipeline_kwargs,
+                          anti_ci: bool = False,
+                          n_trials: int = 770,
+                          n_scales: int = 5,
+                          sigma: int = 5,
+                          noise_type: str = 'sinusoid',
+                          seed: int = 1,
+                          save_ci: bool = True,
+                          save_zmap: bool = True,
+                          experiment_path:str='./experiment',
+                          label:str='rcpyci'):
+        # pass along input variables apart from already consumed ones
+        kwargs = {k: v for k, v in locals().items()}
+        consumed_variables = ['condition', 'data', 'save_ci', 'save_zmap', 'experiment_path', 'label']
+        for key in consumed_variables:
+            kwargs.pop(key, None)
+        # Sort the dataframe by 'condition', 'participant_id', and 'stimulus_id'
+        sorted_data = data.sort_values(by=['condition', 'participant_id', 'stimulus_id'])
+        # Calculate the average response for each stimulus_id across participants within each condition
+        grouped_data = sorted_data.groupby(['condition', 'stimulus_id'])['responses'].mean().reset_index()
+        # extract data for a single condition and sort by stimulus_id
+        single_condition_data = grouped_data[grouped_data['condition'] == condition].sort_values(by='stimulus_id')
+        sorted_responses = single_condition_data['responses'].to_numpy().reshape(770, 1)
+        kwargs['responses'] = sorted_responses
+        
+        ci, combined, zmap = compute_ci_and_zmap(**kwargs)
+        
+        ci_filename = f"ci_{label}_{condition}.png"
+        if anti_ci:
+            ci_filename = f"antici_{label}_{condition}.png"
+        
+        zmap_filename = f"zmap_{label}_{condition}.png"
+
+        if save_ci:
+            save_image(image=combined, path=os.path.join(experiment_path, "ci", ci_filename), save_npy=True)
+        if zmap is not None and save_zmap:
+            save_image(image=zmap, path=os.path.join(experiment_path, "zmap", zmap_filename), save_npy=True)
+
+        return condition, ci, combined, zmap
+
+    def process_conditions(conditions, 
+                           data: pd.DataFrame,
+                           base_image: np.ndarray,
+                           stimuli_params: np.ndarray = None,
+                           ci_postproc_pipe: Callable[[Any], Any] = ci_postprocessing_pipeline,
+                           ci_postproc_kwargs: dict = default_ci_postprocessing_pipeline_kwargs,
+                           zmap_pipe: Callable[[Any], Any] = compute_zmap_ttest_pipeline,
+                           zmap_kwargs: dict = default_compute_zmap_ttest_pipeline_kwargs,
+                           anti_ci: bool = False,
+                           n_trials: int = 770,
+                           n_scales: int = 5,
+                           sigma: int = 5,
+                           noise_type: str = 'sinusoid',
+                           seed: int = 1,
+                           save_ci: bool = True,
+                           save_zmap: bool = True,
+                           experiment_path:str='./experiment',
+                           label:str='rcpyci',
+                           n_jobs=10):
+        
+        # pass along input variables apart from already consumed ones
+        kwargs = {k: v for k, v in locals().items()}
+        consumed_variables = ['conditions', 'n_jobs', 'process_participant', 'process_condition']
+        for key in consumed_variables:
+            kwargs.pop(key, None)
+        
+        logging.info("Started calculating ci and zmaps for conditions. "
+                     "Be mindful that with higher parallel jobs the progress bar becomes more inaccurate. "
+                     "This may take a while... ")
+        result = Parallel(n_jobs=n_jobs)(delayed(process_condition)(
+            condition=condition,
+            **kwargs
+        ) for condition in tqdm(conditions))
+
+        logging.info("Finished processing ci and zmaps for conditions.")
+        return result
+
+    @skip_if_exist
+    def process_participant(participant,
+                            data: pd.DataFrame,
+                            base_image: np.ndarray,
+                            stimuli_params: np.ndarray = None,
+                            ci_postproc_pipe: Callable[[Any], Any] = ci_postprocessing_pipeline,
+                            ci_postproc_kwargs: dict = default_ci_postprocessing_pipeline_kwargs,
+                            zmap_pipe: Callable[[Any], Any] = compute_zmap_ttest_pipeline,
+                            zmap_kwargs: dict = default_compute_zmap_ttest_pipeline_kwargs,
+                            anti_ci: bool = False,
+                            n_trials: int = 770,
+                            n_scales: int = 5,
+                            sigma: int = 5,
+                            noise_type: str = 'sinusoid',
+                            seed: int = 1,
+                            save_ci: bool = True,
+                            save_zmap: bool = True,
+                            experiment_path:str='./experiment',
+                            label:str='rcpyci'):
+        
+        # pass along input variables apart from already consumed ones
+        kwargs = {k: v for k, v in locals().items()}
+        consumed_variables = ['participant', 'data', 'save_ci', 'save_zmap', 'experiment_path', 'label']
+        for key in consumed_variables:
+            kwargs.pop(key, None)
+        
+        # get normalized order of responses for the participant and pass to kwargs
+        sorted_data = data[data['participant_id'] == participant].sort_values(by='stimulus_id')
+        sorted_responses = sorted_data['responses'].to_numpy().reshape(770, 1)
+        kwargs['responses'] = sorted_responses
+
+        ci, combined, zmap = compute_ci_and_zmap(**kwargs)
+        
+        ci_filename = f"ci_{label}_{participant}.png"
+        if anti_ci:
+            ci_filename = f"antici_{label}_{participant}.png"
+        
+        zmap_filename = f"zmap_{label}_{participant}.png"
+
+        if save_ci:
+            save_image(image=combined, path=os.path.join(experiment_path, "ci", ci_filename), save_npy=True)
+        if zmap is not None and save_zmap:
+            save_image(image=zmap, path=os.path.join(experiment_path, "zmap", zmap_filename), save_npy=True)
+
+        return participant, ci, combined, zmap
+    
+    def process_participants(participants, 
+                             data: pd.DataFrame,
+                             base_image: np.ndarray,
+                             stimuli_params: np.ndarray = None,
+                             ci_postproc_pipe: Callable[[Any], Any] = ci_postprocessing_pipeline,
+                             ci_postproc_kwargs: dict = default_ci_postprocessing_pipeline_kwargs,
+                             zmap_pipe: Callable[[Any], Any] = compute_zmap_ttest_pipeline,
+                             zmap_kwargs: dict = default_compute_zmap_ttest_pipeline_kwargs,
+                             anti_ci: bool = False,
+                             n_trials: int = 770,
+                             n_scales: int = 5,
+                             sigma: int = 5,
+                             noise_type: str = 'sinusoid',
+                             seed: int = 1,
+                             save_ci: bool = True,
+                             save_zmap: bool = True,
+                             experiment_path:str='./experiment',
+                             label:str='rcpyci',
+                             n_jobs=10):
+        
+        # pass along input variables apart from already consumed ones
+        kwargs = {k: v for k, v in locals().items()}
+        consumed_variables = ['participants', 'n_jobs', 'process_participant', 'process_condition']
+        for key in consumed_variables:
+            kwargs.pop(key, None)
+        
+        logging.info("Started calculating ci and zmaps for individual participants. "
+                     "Be mindful that with higher parallel jobs the progress bar becomes more inaccurate. "
+                     "This may take a while... ")
+        result = Parallel(n_jobs=n_jobs)(delayed(process_participant)(
+            participant=participant,
+            **kwargs
+        ) for participant in tqdm(participants))
+
+        logging.info("Finished processing individual ci and zmaps.")
+        return result
+
+    base_image = read_image(os.path.join(os.getcwd(), base_face_path), grayscale=True)
+    os.makedirs(experiment_path, exist_ok=True)
+
+    # pass along input variables apart from already consumed ones
+    kwargs = {k: v for k, v in locals().items()}
+    consumed_variables = ['base_face_path', 'process_participants', 'process_participant', 'process_conditions', 'process_condition']
+    for key in consumed_variables:
+        kwargs.pop(key, None)
+    
+    participants = list(data['participant_id'].unique())
+    kwargs['participants'] = participants
+    participants_results = process_participants(**kwargs)
+    kwargs.pop('participants', None)
+
+    conditions = list(data['condition'].unique())
+    kwargs['conditions'] = conditions
+    conditions_results = process_conditions(**kwargs)
+    
+    return participants_results, conditions_results
+
+
 def setup_experiment(base_face_path: str,
                     n_trials:int = 770,
                     n_scales:int=5,
