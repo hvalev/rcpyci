915970bb68367b95263df13cd67305b9ede75ea2
Hristo Valev <h_valev@yahoo.de>
2024-05-16 14:16:21 +0200

    fix for creating sample data

--- rcpyci/utils.py
+++ rcpyci/utils.py
@@ -4,11 +4,12 @@ Description: object to object part of the library, for any interaction and inter
 """
 import os
 import random
+from functools import wraps
 
 import numpy as np
 import pandas as pd
 from PIL import Image
-from functools import wraps
+
 
 def skip_if_exist(func):
     @wraps(func)
@@ -50,7 +51,7 @@ def skip_if_exist(func):
     
     return wrapper
 
-def create_test_data(num_participants:int=100, num_stimulus:int=770):
+def create_test_data(n_participants:int=100, n_trials:int=770):
     # for sample data reproducibility
     seed = 42
     np.random.seed(seed)
@@ -58,12 +59,11 @@ def create_test_data(num_participants:int=100, num_stimulus:int=770):
 
     conditions = ["cond1", "cond2", "cond3", "cond4", "cond5"]
     data = {
-        'idx': np.arange(0, num_participants*num_stimulus),
-        'condition': np.concatenate([np.tile(np.random.choice(conditions), num_stimulus) for _ in range(num_participants)]),
-        'participant_id': np.repeat(np.arange(0, num_participants), num_stimulus),
-        'stimulus_id': np.tile(np.arange(0, num_stimulus), num_participants),
-        'stimulus_presentation_order': np.concatenate([np.random.permutation(num_stimulus) for _ in range(num_participants)]),
-        'responses': np.random.choice([-1, 1], size=num_participants*num_stimulus).astype(int),
+        'idx': np.arange(0, n_participants*n_trials),
+        'condition': np.concatenate([np.tile(np.random.choice(conditions), n_trials) for _ in range(n_participants)]),
+        'participant_id': np.repeat(np.arange(0, n_participants), n_trials),
+        'stimulus_id': np.tile(np.arange(0, n_trials), n_participants),
+        'responses': np.random.choice([-1, 1], size=n_participants*n_trials).astype(int),
     }
     return pd.DataFrame(data)
 
@@ -95,18 +95,11 @@ def verify_data(df: pd.DataFrame, verbose=False):
     vprint(df['condition'].value_counts(), verbose)
     if len(df['condition'].value_counts().unique()) != 1:
         print('WARNING conditions are not even between participants')
-    
 
     vprint("\nDistribution of stimulus IDs:", verbose)
     vprint(df['stimulus_id'].value_counts(), verbose)
     if len(df['stimulus_id'].value_counts().unique()) != 1:
         print('ERROR not all stimuli have been presented evenly to participants')
-
-    if 'stimulus_presentation_order' in df.columns:
-        vprint("\nDistribution of stimulus IDs:", verbose)
-        vprint(df['stimulus_presentation_order'].value_counts(), verbose)
-        if len(df['stimulus_presentation_order'].value_counts().unique()) != 1:
-            print('ERROR presentation order id not congruent with stimuli')
 
     # Step 5: Assess response distribution
     vprint("\nDistribution of responses:", verbose)
@@ -115,3 +108,50 @@ def verify_data(df: pd.DataFrame, verbose=False):
     if not (len(responses) == 2 and -1 in responses and 1 in responses):
         print('ERROR: Found a value in responses different from 1 or -1')
 
+# def compare_images(img1_path, img2_path):
+#     return all([x == y for x, y in zip(Image.open(img1_path).convert("L").getdata(), Image.open(img2_path).convert("L").getdata())])
+
+
+def compare_images(img1_path, img2_path):
+    # Open images
+    img1 = Image.open(img1_path)
+    img2 = Image.open(img2_path)
+
+    # Check if images have the same size
+    if img1.size != img2.size:
+        print("Images have different dimensions.")
+        return False
+
+    # Convert images to grayscale if needed
+    img1 = img1.convert("L")
+    img2 = img2.convert("L")
+
+    # Compare pixel values
+    width, height = img1.size
+    for x in range(width):
+        for y in range(height):
+            if img1.getpixel((x, y)) != img2.getpixel((x, y)):
+                print("Images are not equivalent.")
+                return False
+
+    # If no differing pixels are found, images are equivalent
+    print("Images are equivalent.")
+    return True
+
+def compare_images_in_folders(folder1, folder2):
+    # List PNG files in both folders
+    png_files1 = [file for file in os.listdir(folder1) if file.endswith('.png')]
+    png_files2 = [file for file in os.listdir(folder2) if file.endswith('.png')]
+
+    # Iterate over PNG files in folder1
+    for file1 in png_files1:
+        # Check if there is an equivalent file in folder2
+        file2 = file1
+        if file2 in png_files2:
+            img1_path = os.path.join(folder1, file1)
+            img2_path = os.path.join(folder2, file2)
+            print(f"Comparing {file1} and {file2}:")
+            if not compare_images(img1_path, img2_path):
+                return False
+        else:
+            print(f"No equivalent file found for {file1} in folder2.")
