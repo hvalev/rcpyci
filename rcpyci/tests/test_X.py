88178277661563f7ab7463c0d153d8f1e53f42f6
Hristo Valev <h_valev@yahoo.de>
2024-05-20 20:25:36 +0200

    moved tests and fixed readme

--- rcpyci/tests/test_X.py
+++
@@ -1,4 +0,0 @@
-# TODO add tests for functions
-# TODO add a pyproject.toml
-# TODO add ref implementation as [dev] dependency
-# TODO idempotence tests of current against the reference implementation
---
+++ tests/test_X.py
@@ -0,0 +1,2 @@
+# TODO idempotence tests of current against the reference implementation
+# TODO sideload using the __generate_stimuli with seed priming 
